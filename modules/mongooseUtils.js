f960daa96539b745a53d9c5a68bd330eea5fa7b8
Pechenyice <german.iskhakov@mail.ru>
2021-06-19 23:24:42 +0300

    db queries logic updats

--- modules/mongooseUtils.js
+++ modules/mongooseUtils.js
@@ -2,21 +2,17 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 require('dotenv').config();
- 
-// const clientSchema = new Schema({vkId: Number});
-// const client = mongoose.model("Client", userSchema);
- 
-// mongoose.connect("mongodb://localhost:27017/usersdb", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, function(err){
-//     if(err) return console.log(err);
-// });
 
 let mongooseUtils = {
     clientSchema: null,
     client: null,
 
+    /**
+     * start mongo db session.
+     */
     startSession: function() {
 
-        this.clientSchema = new Schema({vkId: Number});
+        this.clientSchema = new Schema({id: Number, ttsRoots: Boolean, tts: Boolean, jokes: Boolean, rand: Boolean});
         this.client = mongoose.model("client", this.clientSchema);
 
         mongoose.connect(process.env.MONGODBURI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, function(err){
@@ -24,15 +20,65 @@ let mongooseUtils = {
         });
     },
 
+    /**
+     * end mongo db session.
+     */
+    endSession: function() {
+        mongoose.disconnect();
+    },
+
+    /**
+     * save client to collection.
+     * @param {Object} user - New user.
+     * @param {Number} user.id - user vk id.
+     * @param {Boolean} user.ttsRoots - user tts access.
+     * @param {Boolean} user.tts - user tts mode.
+     * @param {Boolean} user.jokes - user jokes access.
+     * @param {Boolean} user.rand - user rand access.
+     */
     saveClient: function(user) {
-        let client = new this.client({vkId: user.vkId});
-        
-        console.log(client)
+        let client = new this.client(user);
         client.save(function(err){
             if(err) return console.log(err);
             console.log("saved: " + client)
         });
+    },
+
+    /**
+     * get all clients vk ids from collection.
+     * @returns {Number[]}
+     */
+    getClientsIds: async function() {
+        let ids = [];
+        await this.client.find({}, function(err, clients){        
+            if(err) return console.log(err);
+            for (let c of clients) {
+                ids.push(c.id);
+            }
+        });
+        return ids;
+    },
+
+    /**
+     * get all clients vk ids from collection.
+     * @returns {Object[]}
+     */
+     getClientsModes: async function() {
+        let ids = {};
+        await this.client.find({}, function(err, clients){        
+            if(err) return console.log(err);
+            for (let c of clients) {
+                ids[c.id] = {
+                    'ttsRoots': c.ttsRoots,
+                    'tts': c.tts,
+                    'jokes': c.jokes,
+                    'rand': c.rand,
+                };
+            }
+        });
+        return ids;
     }
+
 }
 
 module.exports = mongooseUtils;
