c3113ad9feb90f952365afdca37ac8731e8579d3
Pechenyice <german.iskhakov@mail.ru>
2021-06-20 23:12:49 +0300

    tests updates

--- tests/test_saveClient.js
+++ tests/test_saveClient.js
@@ -1,5 +1,4 @@
 const mongooseUtils = require('./../modules/mongooseUtils.js');
 
 mongooseUtils.startSession();
-// mongooseUtils.saveClient({id: 172576383, ttsRoots: true, tts: false, jokes: true, rand: true});
-mongooseUtils.saveClient({id: 210062887, ttsRoots: true, tts: false, jokes: true, rand: true});
+mongooseUtils.saveClient({id: 161894117, ttsRoots: true, tts: false, jokes: true, rand: true});
