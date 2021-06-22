2784b8cef94da7eca13d02901e0ddec694aa72ad
Pechenyice <german.iskhakov@mail.ru>
2021-06-22 23:08:52 +0300

    migrations are working now

--- migrations/1624299041596-newModeChat.js
+++ migrations/1624299041596-newModeChat.js
@@ -4,14 +4,20 @@ require('./../models/client.js');
  */
 async function up () {
   // Write migration here
-  this('client').updateMany({}, {'chat': {$exists : false}}, {$set: {'chat': true}});
+  this('client').updateMany({}, {chat: true}, function(err, result){
+    if(err) return console.log(err);
+    console.log(result);
+});
 }
 
 /**
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 async function down () {
-  this('client').update({"chat": true}, {"$set":{"chat": false}}, {"multi": true});
+  this('client').updateMany({}, {chat: undefined}, function(err, result){
+      if(err) return console.log(err);
+      console.log(result);
+  });
 }
 
 module.exports = { up, down };
